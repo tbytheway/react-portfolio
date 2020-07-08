48ead3bc75a500448a37c0fb037d98bf4660e896
tbytheway <tdbytheway@gmail.com>
2020-07-08 16:21:30 -0600

    react constructor overview

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -3,6 +3,12 @@ import React, { Component } from "react";
 import PortfolioItem from "./portfolio-item";
 
 export default class PortfolioContainer extends Component {
+    constructor() {
+      super()
+
+
+      console.log("Portfolio container has rendered")
+    }
   // State
   // Lifecycle hooks
   render() {
